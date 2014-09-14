dbbcaf63eb4f82a679b2395737fac4a404c2c673
nlp hacker <nlphacker@outlook.com>
2014-09-14 21:32:11 +0800

    build zh-cn

---
+++ build.sh
@@ -0,0 +1,5 @@
+mvn install
+cp ~/.m2/repository/org/atilika/kuromoji/kuromoji/0.7.7/kuromoji-0.7.7.jar  target/marytts-5.2-SNAPSHOT/lib/
+cp ~/.m2/repository/com/chenlb/mmseg4j/mmseg4j-core/1.10.0/mmseg4j-core-1.10.0.jar  target/marytts-5.2-SNAPSHOT/lib/
+cp ~/.m2/repository/com/belerweb/pinyin4j/2.5.0/pinyin4j-2.5.0.jar  target/marytts-5.2-SNAPSHOT/lib/
+
